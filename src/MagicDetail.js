430880bfad82878e1f50d7973f5f66e4d38b4627
David <Rigby335@hotmail.com>
2022-06-23 10:00:41 -0700

    useEffect to getSingle row from table

--- src/MagicDetail.js
+++ src/MagicDetail.js
@@ -1,6 +1,23 @@
 // uses the correct react-router-dom hook to get the id from the URL, then uses that id to fetch the correct item
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getSingleMagicCard } from './services/fetch-utils';
+
 
 export default function MagicDetail() {
+  
+  const [magicCards, setMagicCards] = useState([]);
+
+  const params = useParams();
+
+  useEffect(() => {
+    async function goFetch() {
+      const data = await getSingleMagicCard(params.id);
+      setMagicCards(data);
+    }
+    goFetch();
+  }, [params.id]);
+
   return (
     <div>
 
